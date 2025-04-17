731ebdb499fe6107b2298d18d89bf2589244ab0a
mmdlash <m4m4d.80@gmail.com>
2025-04-17 11:55:30 +0330

    Update

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,5 @@ export default {
   server: {
     port: 5173,
     open: true
-  },
-  optimizeDeps: {
-    include: ["@walletconnect/modal-sign-html"]
-  },
-  ssr: {
-    noExternal: ["@walletconnect/modal-sign-html"]
   }
-}
+};
